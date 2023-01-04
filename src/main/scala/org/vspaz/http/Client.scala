c5c8971570cd7a235740a4bd4c67a7a065c1eb85
vspaz <vladimir.edelman@gmail.com>
2023-01-04 21:05:37 +0100

    add function 'buildRequest'

--- src/main/scala/org/vspaz/http/Client.scala
+++ src/main/scala/org/vspaz/http/Client.scala
@@ -2,12 +2,14 @@ package org.vspaz.http
 
 import scala.concurrent.duration.{Duration, SECONDS}
 import sttp.client3._
+import sttp.model.Method
 
 class Client(
   host: String = "",
   userAgent: String = "",
   basicAuthUser: String = "",
   basicUserPassword: String = "",
+  token: String = "",
   retryCount: Int = 3,
   retryOnErrors: Set[Int] = Set(),
   delay: Int = 2,
@@ -22,6 +24,17 @@ class Client(
       SttpBackendOptions.connectionTimeout(Duration(connectionTimeout, SECONDS))
     )
   );
+  def buildRequest(method: String, uri: String): RequestT[Identity, String, Any] = {
+    val url = host + uri
+    val request = basicRequest
+      .headers(Map("User-Agent" -> userAgent))
+      .readTimeout(responseTimeout)
+      .method(Method(method.capitalize), uri = uri"$host$url")
+      .response(asStringAlways)
+    if (basicAuthUser != "" && basicUserPassword != "")
+      request.auth.basic(basicAuthUser, basicUserPassword)
+    request
+  }
 
   override def toString: String =
     s"host: '$host', userAgent: '$userAgent', readTimeout: '$readTimeout', 'connectionTimeout: '$connectionTimeout'"
