4f32cc55db769e150f3fdc4cb97b5d2604c44cd0
vspaz <vladimir.edelman@gmail.com>
2023-01-04 20:23:13 +0100

    update scalafmt

--- src/main/scala/org/vspaz/http/Client.scala
+++ src/main/scala/org/vspaz/http/Client.scala
@@ -4,28 +4,25 @@ import scala.concurrent.duration.{Duration, SECONDS}
 import sttp.client3._
 
 class Client(
-    host: String = "",
-    userAgent: String = "",
-    basicAuthUser: String = "",
-    basicUserPassword: String = "",
-    retryCount: Int = 3,
-    retryOnErrors: Set[Int] = Set(),
-    delay: Int = 2,
-    readTimeout: Int = 10,
-    connectionTimeout: Int = 10,
-    logger: Option[ILogger] = None,
-    backend: Option[SttpBackend[Identity, Any]] = None
+  host: String = "",
+  userAgent: String = "",
+  basicAuthUser: String = "",
+  basicUserPassword: String = "",
+  retryCount: Int = 3,
+  retryOnErrors: Set[Int] = Set(),
+  delay: Int = 2,
+  readTimeout: Int = 10,
+  connectionTimeout: Int = 10,
+  logger: Option[ILogger] = None,
+  backend: Option[SttpBackend[Identity, Any]] = None
 ) {
   private val responseTimeout = Duration(readTimeout, SECONDS);
   private val http = backend.getOrElse(
-    HttpClientSyncBackend(
-      options = SttpBackendOptions.connectionTimeout(
-        Duration(connectionTimeout, SECONDS)
-      )
+    HttpClientSyncBackend(options =
+      SttpBackendOptions.connectionTimeout(Duration(connectionTimeout, SECONDS))
     )
   );
 
-  override def toString: String = {
+  override def toString: String =
     s"host: '$host', userAgent: '$userAgent', readTimeout: '$readTimeout', 'connectionTimeout: '$connectionTimeout'"
-  }
 }
