9dc708347b57f5af32b5e80653e24b449d49c07f
vspaz <vladimir.edelman@gmail.com>
2023-01-04 19:39:36 +0100

    add trait ILogger

--- src/main/scala/org/vspaz/http/ILogger.scala
+++ src/main/scala/org/vspaz/http/ILogger.scala
@@ -1,5 +1,9 @@
 package org.vspaz.http
 
 trait ILogger {
-
+  def error(message: String*): Unit
+  def warn(message: String*): Unit
+  def info(message: String*): Unit
+  def debug(message: String*): Unit
+  def trace(message: String*): Unit
 }
