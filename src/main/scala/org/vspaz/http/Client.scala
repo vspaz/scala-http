f69738f5515c5e21e9b6373563900e91f7137f0b
vspaz <vladimir.edelman@gmail.com>
2023-01-04 18:51:00 +0100

    add constructor with default values

--- src/main/scala/org/vspaz/http/Client.scala
+++ src/main/scala/org/vspaz/http/Client.scala
@@ -1,5 +1,13 @@
 package org.vspaz.http
 
-object Client {
-
-}
+class Client(
+    host: String = "",
+    userAgent: String = "",
+    basicAuthUser: String = "",
+    basicUserPassword: String = "",
+    retryCount: Int = 3,
+    retryOnErrors: List[Int] = List(),
+    delay: Int = 2,
+    readTimeout: Int = 10,
+    connectionTimeout: Int = 10
+) {}
