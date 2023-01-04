370a0abd04e745c324d26f05223cf4e640ec7138
vspaz <vladimir.edelman@gmail.com>
2023-01-04 19:05:51 +0100

    add timeouts

--- src/main/scala/org/vspaz/http/Client.scala
+++ src/main/scala/org/vspaz/http/Client.scala
@@ -1,13 +1,19 @@
 package org.vspaz.http
 
+import scala.concurrent.duration.{Duration, SECONDS};
+
+
 class Client(
     host: String = "",
     userAgent: String = "",
     basicAuthUser: String = "",
     basicUserPassword: String = "",
     retryCount: Int = 3,
-    retryOnErrors: List[Int] = List(),
+    retryOnErrors: Set[Int] = Set(),
     delay: Int = 2,
     readTimeout: Int = 10,
     connectionTimeout: Int = 10
-) {}
+) {
+  private val responseTimeout = Duration(readTimeout, SECONDS);
+  private val connTimeout = Duration(connectionTimeout, SECONDS);
+}
