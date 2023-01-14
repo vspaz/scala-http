4ff2be36252aea6635a27059c6486664db7d4457
vspaz <vladimir.edelman@gmail.com>
2023-01-14 13:13:35 +0100

    add example to Main

--- src/main/scala/org/vspaz/Main.scala
+++ src/main/scala/org/vspaz/Main.scala
@@ -3,38 +3,20 @@ package org.vspaz
 import org.junit.jupiter.api.Assertions.{assertEquals, assertTrue}
 import org.vspaz.http.Client
 
+case class Response(
+                     args: Option[Map[String, String]],
+                     headers: Option[Map[String, String]],
+                     origin: Option[String],
+                     url: Option[String]) {}
+
 object Main {
-  def main(args: Array[String]): Unit = {
-    val client = new Client(
-      host= "https://example.com",
-      userAgent="client-name-and-version",
-      basicAuthUser="user",
-      basicUserPassword = "pass",
-      retryCount = 3,
-      retryOnErrors = Set(400, 500, 503),
-      retryOnExceptions = Set(
-        "sttp.client3.SttpClientException.TimeoutException",
-        "java.lang.RuntimeException",
-        "java.lang.Throwable",
-      ),
-      retryDelay = 1,
-      connectionTimeout = 5,
-      readTimeout = 10,
-    )
-
-    var resp = client.doGet("/some-endpoint")
-
-    // or simply
-    resp = new Client().doGet("https://example.com/some-endpoint")
+  def doSimpleGetExample(): Unit = {
+    val resp = new Client().doGet(endpoint="https://httpbin.org/get")
     assertTrue(resp.isOk())
-    assertEquals(200, resp.statusCode)
-
-    // POST
-    resp = client.doPost(
-      endpoint = "/test-post-method",
-      headers = Map("Content-Type" -> "application/json"),
-      payload = Map("header" -> "value")
-    )
-    assertTrue(resp.isSuccess())
+    val decodedBody = resp.fromJson(classOf[Response])
+    assertEquals("https://httpbin.org/get", decodedBody.url.get)
+  }
+  def main(args: Array[String]): Unit = {
+    doSimpleGetExample()
   }
 }
